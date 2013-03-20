5f720a68fe396155f179abf68c3d9ee27ea84f6e
tbeek <timtebeek@gmail.com>
2013-03-20 09:37:55 +0100

    Add breakdancer code & makefile

---
+++ breakdancer/breakdancer2vcf.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+'''
+Convert breakdancer output to pseudo .vcf file format.
+
+Created on Mar 18, 2013
+
+@author: tbeek
+'''
+
+import csv
+import os.path
+import sys
+
+
+def main(tsvfile, vcffile):
+    '''
+    
+    :param tsvfile: filename of input file.tsv
+    :type tsvfile: string
+    :param vcffile: filename of output file.vcf
+    :type vcffile: string
+    '''
+    with open(tsvfile) as reader:
+        # Parse file
+        dictreader = _parse_tsvfile(reader)
+        print dictreader.fieldnames
+
+        # Write out file
+        _format_vcffile(dictreader, vcffile)
+
+    # Quick output
+    with open(vcffile) as reader:
+        print reader.read(1000)
+
+def _parse_tsvfile(readable):
+    '''
+    Read readable using csv.Sniffer and csv.DictReader
+    :param readable: open file.tsv handle to read with csv.DictReader
+    :type readable: file
+    '''
+    prev, curr = 0, 0
+    while True:
+        line = readable.readline()
+        if not line.startswith('#'):
+            readable.seek(prev + 1)
+            break
+        else:
+            prev = curr
+            curr = readable.tell()
+
+    # Determine dialect
+    curr = readable.tell()
+    dialect = csv.Sniffer().sniff(readable.read(1000))
+    readable.seek(curr)
+
+    # Read file
+    dictreader = csv.DictReader(readable, dialect=dialect)
+    return dictreader
+
+
+_tsv_fields = ('Chr1', 'Pos1', 'Orientation1',
+               'Chr2', 'Pos2', 'Orientation2',
+               'Type', 'Size', 'Score',
+               'num_Reads', 'num_Reads_lib',
+               'ERR031544.sort.bam')
+# 'Chr1': '1',
+# 'Pos1': '269907',
+# 'Orientation1': '39+39-',
+# 'Chr2': '1',
+# 'Pos2': '270919',
+# 'Orientation2': '39+39-',
+# 'Type': 'DEL',
+# 'Size': '99',
+# 'Score': '99',
+# 'num_Reads': '38',
+# 'num_Reads_lib': '/home/allbio/ERR031544.sort.bam|38',
+# 'ERR031544.sort.bam': 'NA'
+
+_vcf_fields = ('CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUALITY', 'FILTER', 'INFO')
+
+def _format_vcffile(dictreader, vcffile):
+    '''
+    Create a pseudo .vcf file based on values read from DictReader instance.
+    :param dictreader: DictReader instance to read data from
+    :type dictreader: csv.DictRedaer
+    :param vcffile: output file.vcf filename
+    :type vcffile: string
+    '''
+    with open(vcffile, mode='w')as writer:
+        writer.write('#{}\n'.format('\t'.join(_vcf_fields)))
+        for line in dictreader:
+            CHROM = line['Chr1']
+            # TODO Figure out whether we have zero or one based positioning
+            POS = int(line['Pos1'])
+            INFO = 'PROGRAM=breakdancer;SVTYPE={};SVLEN={}'.format(line['Type'],
+                                                                   0 - int(line['Size']))
+
+            # Create record
+            output = '{}\t{}\t.\t.\t.\t.\tPASS\t{}\n'.format(CHROM, POS, INFO)
+
+            # Write record
+            writer.write(output)
+
+
+if __name__ == '__main__':
+    _tsvfile = 'data/ERR031544.sort.bam.tsv'
+    if len(sys.argv) == 2:
+        _tsvfile = sys.argv[1]
+    _vcffile = os.path.splitext(_tsvfile)[0] + '.vcf'
+    main(_tsvfile, _vcffile)
