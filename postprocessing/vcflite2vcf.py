7e3037a0152f52c6ce16408307bd559f0c14794b
Leung <wyleung@5-KG-12-0155.lumcnet.prod.intern>
2013-03-28 10:54:25 +0100

    VCFlite to VCF convertor (allbio specific)
    Commit with code for DEL sv types

--- postprocessing/vcflite2vcf.py
+++ postprocessing/vcflite2vcf.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# vcflite2vcf.py
+# 
+__doc__ = """
+Takes an incomplete VCF, and tries to complement the ref and alt fields (not for all cases)
+Information should be stored in the INFO fields
+"""
+__author__ = "Wai Yi Leung"
+__contact__ = "w dot y dot leung strudelsign lumc dot nl"
+# (c) 2013 by Wai Yi Leung [LUMC - SASC]
 
 import re
 import argparse
 import os
 import vcf
 
-"""
-Takes an incomplete VCF, and tries to complement the ref and alt fields (not for all cases)
-Information should be stored in the INFO fields
-"""
-
 class VCFLite2VCF(object):
-    def __init__( self, pindelfile, reference ):
+    def __init__( self, reference, outputfile=None ):
         self.readFasta( reference )
+        self.vcfoutput = '/tmp/out.vcf'
+        if outputfile:
+            self.vcfoutput = outputfile
+        print self.vcfoutput
     
     def readFasta( self, sFile, *args, **kwargs ):
         """
@@ -24,9 +34,7 @@ class VCFLite2VCF(object):
         
         with open(sFile,'r') as fd:
             for name_raw, seq in self.read_fasta(fd):
-#                 print name_raw
                 name = name_raw.replace('>','').split(' ')[0]
-#                 print name
                 self.reference[ name ] = seq
 
     #http://stackoverflow.com/questions/7654971/parsing-a-fasta-file-using-a-generator-python
@@ -42,47 +50,44 @@ class VCFLite2VCF(object):
         if name: yield (name, ''.join(seq))
 
     def getrefbase( self, chromosome, start, stop ):
-            chromosome =  self.reference.get( chromosome, None )
-            return chromosome[ int(start )-1:int(stop)-1 ]
-    
-
-    def svtype( self, sFlag ):
-        return {
-            'D': 'DEL',
-            'I': 'INS',
-        }.get( sFlag )
+        # expect coordinates in vcf file to be 1 indexed
+        chromosome =  self.reference.get( chromosome, None )
+        return chromosome[ int(start )-1:int(stop)-1 ]
 
     def parseFile( self, sFile, *args, **kwargs ):
         """
+            Parse the vcflite file with PyVCF (0.6.3) 
+            pip install git+git://github.com/jamescasbon/PyVCF.git@6280a655b4#egg=PyVCF
         """
         vcf_reader = vcf.Reader( open( sFile, 'r' ) )
-        vcf_writer = vcf.Writer( open('/tmp/out.vcf', 'w'), vcf_reader)
+        vcf_writer = vcf.Writer( open( self.vcfoutput, 'w'), vcf_reader)
         for record in vcf_reader:
-        	ALT = record.ALT
-        	record.REF = self.getrefbase( record.CHROM, record.start, record.end )
-        	record.ALT = [self.getrefbase( record.CHROM, record.start, record.end )]
-        	if 'INS' in record.INFO['SVTYPE']:
-	        	SVLEN = abs(int(record.INFO['SVLEN'][0]))
-	        	SVseq = SVLEN * "N"
-	        	record.ALT = [self.getrefbase( record.CHROM, record.start, record.end )+SVseq]
-	        	print record
-	        	print record.INFO
+            record.REF = self.getrefbase( record.CHROM, record.start, record.end )
+            record.ALT = [self.getrefbase( record.CHROM, record.start, record.end )]
 
-	        vcf_writer.write_record(record)
+            if 'INS' in record.INFO['SVTYPE']:
+                SVLEN = abs(int(record.INFO['SVLEN'][0]))
+                SVseq = SVLEN * "N"
+                record.ALT = [self.getrefbase( record.CHROM, record.start, record.end )+SVseq]
+            elif 'DEL' in record.INFO['SVTYPE']:
+                record.ALT = [self.getrefbase( record.CHROM, record.start, record.start+1 )]
+            vcf_writer.write_record(record)
 
 if __name__ == '__main__':
     """
-        This script will print all output to commandline
+        This script will print all output to commandline (/dev/stdout) or to -o
     """
     parser = argparse.ArgumentParser()
     # args for first pair
     parser.add_argument('-f', '--file', dest='vcflite', type=str,
-            help='Pindel file')
+            help='VCF lite file')
+    parser.add_argument('-o', '--outputvcf', dest='outputvcf', type=str,
+            help='Output vcf to')
     parser.add_argument('-r', '--reference', dest='reference', type=str,
             help='reference file')
 
     args = parser.parse_args()
-    parser = VCFLite2VCF( args.vcflite, args.reference )
+    parser = VCFLite2VCF( args.reference, args.outputvcf )
     parser.parseFile( args.vcflite )
 
 
